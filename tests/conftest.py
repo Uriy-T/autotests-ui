c9e2025281f7f114728423b5ae27296bacd8d8cf
Uriy-T <briner_asterios@mail.ru>
2025-10-27 00:14:45 +0400

    6.7 Параметризация Pytest. Практика работы с pytest параметризацией

--- tests/conftest.py
+++ tests/conftest.py
@@ -30,3 +30,9 @@ def chromium_page_with_state(initialize_browser_state, playwright: Playwright) -
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context(storage_state='browser-state.json')
     yield context.new_page()
+
+@pytest.fixture(scope='function')
+def chromium_page(playwright: Playwright) -> Page:
+    browser = playwright.chromium.launch(headless=False)
+    yield browser.new_page()
+    browser.close()
