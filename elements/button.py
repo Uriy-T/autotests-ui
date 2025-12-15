0f6dc828d9a5507b51d023c519d131ae8694d12d
Uriy-T <briner_asterios@mail.ru>
2025-12-15 23:47:55 +0400

    11.3 Практикуемся в интеграции покрытия в UI тесты. Практическое задание: Работа с инструментом ui-coverage-tool

--- elements/button.py
+++ elements/button.py
@@ -1,4 +1,6 @@
 from playwright.sync_api import expect
+from ui_coverage_tool import ActionType
+
 from elements.base_element import BaseElement
 import allure
 
@@ -20,9 +22,13 @@ class Button(BaseElement):
             logger.info(step)
             expect(locator).to_be_enabled()
 
+        self.track_coverage(ActionType.ENABLED, nth, **kwargs)
+
     def check_disabled(self, nth: int = 0, **kwargs):
         step = f'Checking that {self.type_of} "{self.name}" is disabled'
         with allure.step(step):
             locator = self.get_locator(nth, **kwargs)
             logger.info(step)
             expect(locator).to_be_disabled()
+
+        self.track_coverage(ActionType.DISABLED, nth, **kwargs)
--- elements/text_area.py
+++ elements/text_area.py
@@ -1,4 +1,5 @@
 from playwright.sync_api import expect, Locator
+from ui_coverage_tool import ActionType
 
 from elements.base_element import BaseElement
 import allure
@@ -17,6 +18,9 @@ class TextArea(BaseElement):
     def get_locator(self, nth: int = 0, **kwargs) -> Locator:
         return super().get_locator(nth, **kwargs).locator('textarea').first
 
+    def get_raw_locator(self, nth: int = 0, **kwargs) -> str:
+        return f"{super().get_locator(nth, **kwargs).locator('textarea').first}"
+
     def fill(self, value: str, nth: int = 0, **kwargs):
         step = f'Fill {self.type_of} "{self.name}" to value "{value}"'
         with allure.step(step):
@@ -24,9 +28,13 @@ class TextArea(BaseElement):
             logger.info(step)
             locator.fill(value)
 
+        self.track_coverage(ActionType.FILL, nth, **kwargs)
+
     def check_have_value(self, value: str, nth: int = 0, **kwargs):
         step = f'Checking that {self.type_of} "{self.name}" to has value "{value}"'
         with allure.step(step):
             locator = self.get_locator(nth, **kwargs)
             logger.info(step)
             expect(locator).to_have_value(value)
+
+        self.track_coverage(ActionType.VALUE, nth, **kwargs)
